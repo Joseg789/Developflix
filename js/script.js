96bacd5d8615af7f6f8b5722ea8b6939f4d31ad0
joseg789 <joseg191.js@gmail.com>
2025-11-05 12:20:25 +0100

    terminando el proyecto

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
-import peliculas from './peliculas.js'
+import peliculas from "./peliculas.js"; // Importa el módulo de películas
 
+//filtramos películas por categoría
+
+//Los generos serán Acción (28), thriller(53), Aventura(12).
+//ACCION
+const action = peliculas.filter((pelicula) => pelicula.genre_ids.includes(28));
+//thriller
+const thriller = peliculas.filter((pelicula) =>
+  pelicula.genre_ids.includes(53)
+);
+const Aventura = peliculas.filter((pelicula) =>
+  pelicula.genre_ids.includes(12)
+);
+//obtenemos elementos del dom
+const peliculasAction = document.getElementById("genero-28");
+const peliculasThriller = document.getElementById("genero-53");
+const peliculasAventura = document.getElementById("genero-12");
+//mostar peliculas
+
+const plantillaPeliculas = (array) => {
+  const htmlPelis = array.map(
+    (peli) =>
+      ` <div>
+        <img src="https://image.tmdb.org/t/p/w200${peli.poster_path}" alt=${peli.original_title}/>
+        <p>${peli.original_title}</p>
+        </div>`
+  );
+
+  return htmlPelis.join(""); //para eliminar las comas
+};
+peliculasAction.innerHTML = plantillaPeliculas(action);
+peliculasThriller.innerHTML = plantillaPeliculas(thriller);
+peliculasAventura.innerHTML = plantillaPeliculas(Aventura);
--- readme.md
+++ readme.md
@@ -11,7 +11,8 @@ Los generos serán Acción (28), thriller(53), Aventura(12). Puedes ver sus ids 
 1. El HTML ya tiene una estructura con la que trabajar
 2. Tienes los scripts para comenzar
 3. Haz que en cada sección aparezca la imagen y el título de la pelicula filtrado por el genero
-4. La base de las imagenes es esta URL `https://image.tmdb.org/t/p/w500` donde añadiremos la ruta relativa que nos ofrece cada una de las películas. Debería quedar algo similar a esto: `https://image.tmdb.org/t/p/w200/qA5kPYZA7FkVvqcEfJRoOy4kpHg.jpg`
+4. La base de las imagenes es esta URL `https://image.tmdb.org/t/p/w500` donde añadiremos la ruta relativa que nos ofrece cada una de las películas. Debería quedar algo similar a esto:
+   `https://image.tmdb.org/t/p/w200/qA5kPYZA7FkVvqcEfJRoOy4kpHg.jpg`
 5. Viene con un diseño sencillo de base. Puedes hacerlo a tu gusto y darle más funcionalidades.
 
 Debería quedar algo similar a esto
@@ -19,6 +20,7 @@ Debería quedar algo similar a esto
 ![Texto Alternativo](./img/developfix.png)
 
 ## Aclaraciones
+
 - Recuerda que usaremos métodos de array e iteraciones
 - También trabajaremos con el DOM para poder mostrar el contenido
 
